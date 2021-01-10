79d56c71aefa2b7241aa7823058afba25228262c
Garrett <greengarrett99@gmail.com>
2021-01-10 00:45:46 -0600

    Start trying gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,18 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "<YOUR_FIREBASE_API_KEY>",
+          authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN>",
+          databaseURL: "<YOUR_FIREBASE_DATABASE_URL>",
+          projectId: "<YOUR_FIREBASE_PROJECT_ID>",
+          storageBucket: "<YOUR_FIREBASE_STORAGE_BUCKET>",
+          messagingSenderId: "<YOUR_FIREBASE_MESSAGING_SENDER_ID>",
+          appId: "<YOUR_FIREBASE_APP_ID>"
+        }
+    }
   ],
 };

3eb7f11a72efd3a2af557a6bb16947fb77639a3e
Garrett <greengarrett99@gmail.com>
2021-01-17 01:24:04 -0600

    Replace index with a placeholder

---
+++ src/views/index-sections/Construction.js
@@ -0,0 +1,74 @@
+import React from "react";
+// import logo from "../assets/img/gfnwa_logo_1.png";
+
+// reactstrap components
+// import { Container, Row, Col } from "reactstrap";
+import {
+    Button,
+    Col,
+    Container,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    UncontrolledDropdown,
+    NavbarBrand,
+    Navbar,
+    NavItem,
+    NavLink,
+    Nav,
+    Row,
+    UncontrolledTooltip,
+  } from "reactstrap";
+import Image from 'react-bootstrap/Image';
+
+
+
+// core components
+
+function Construction() {
+  return (
+    <>
+      <div
+        className="section section-download"
+        id="construction"
+      >
+        <Container className="d-flex justify-content-center">
+            <div className="content-center brand d-flex flex-column w-50 align-items-center">
+                <img
+                    alt="Lydia's Art"
+                    className="align-self-center"
+                    src={require("assets/img/gfnwa_logo_1.png").default}
+                ></img>
+                <h1>Give me a minute! I'm building it!</h1>
+                <h3>Go check out the socials though...</h3>
+                <div class="d-flex flex-row w-100 align-items-center justify-content-around">
+                    <a
+                        href="https://twitter.com/glutenfreenwa"
+                        target="_blank"
+                        id="twitter-tooltip"
+                        >
+                        <i className="fab fa-twitter fa-4x"></i>
+                    </a>
+                    <UncontrolledTooltip target="#twitter-tooltip">
+                        Follow us on Twitter
+                    </UncontrolledTooltip>
+                    <NavLink
+                        href="https://www.instagram.com/glutenfreenwa"
+                        target="_blank"
+                        id="instagram-tooltip"
+                        >
+                        <i className="fab fa-instagram fa-4x"></i>
+                        <p className="d-lg-none d-xl-none">Instagram</p>
+                    </NavLink>
+                    <UncontrolledTooltip target="#instagram-tooltip">
+                        Follow us on Instagram
+                    </UncontrolledTooltip>
+                </div>
+            </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Construction;
